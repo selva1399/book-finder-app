f13b87d82e57889b494f2cd2ff513bd97afa9a3a
selvaganapathy <kselvaganapathy1399@gmail.com>
2024-12-23 09:26:55 +0530

    form field add

--- src/components/book/SearchForm.jsx
+++ src/components/book/SearchForm.jsx
@@ -18,29 +18,40 @@ const SearchForm = (props) => {
         className="container-fluid position-absolute top-50"
         // style={{ height: "100vh" }}
       >
-        <div className="row h-100 justify-content-center align-items-center">
-          <div className="col-10 col-sm-8 col-md-6">
-            <input
-              className="form-control form-control-lg rounded-5 fs-4"
-              style={{ width: "100%" }}
-              type="text"
-              placeholder="Search your book"
-              autoFocus
-              value={searchBook}
-              onChange={(e) => setSearchBook(e.target.value)}
-              required
-            />
+        {" "}
+        <form
+          onSubmit={(e) => {
+            e.preventDefault();
+            if (searchBook.trim() === "") {
+              alert("Please enter a book name.");
+              return;
+            }
+            handleSearch();
+          }}
+        >
+          <div className="row h-100 justify-content-center align-items-center">
+            <div className="col-10 col-sm-8 col-md-6">
+              <input
+                className="form-control form-control-lg rounded-5 fs-4"
+                style={{ width: "100%" }}
+                type="text"
+                placeholder="Search your book"
+                autoFocus
+                value={searchBook}
+                onChange={(e) => setSearchBook(e.target.value)}
+                required
+              />
+            </div>
+            <div className="col-4 col-sm-3 col-md-2 mt-2 mt-sm-0">
+              <button
+                className="btn btn-lg w-100 rounded-5 border-3 border-white fw-bold fs-4 btn-search"
+                style={{ backgroundColor: "#FADA7A" }}
+              >
+                Search
+              </button>
+            </div>
           </div>
-          <div className="col-4 col-sm-3 col-md-2 mt-2 mt-sm-0">
-            <button
-              className="btn btn-lg w-100 rounded-5 border-3 border-white fw-bold fs-4 btn-search"
-              style={{ backgroundColor: "#FADA7A" }}
-              onClick={handleSearch}
-            >
-              Search
-            </button>
-          </div>
-        </div>
+        </form>
       </div>
     </>
   );
